7c271fb4d25d3dd08a40bce74c16473129669438
Jeff Lichty <lich0020@algonquinlive.com>
2019-10-29 15:19:23 -0400

    array, object, numbers testing working

--- robot.test.js
+++ robot.test.js
@@ -7,7 +7,7 @@ describe('Robot module', () => {
         "PLACE 1,2,NORTH", 
         "REPORT"
     ];
-    
+
     // ACT: execute the function to be tested using the control values created above.
     // Calling each of those commands will produce an Array of results
     const result = instructions.map(i => robot.do(i));
@@ -16,9 +16,16 @@ describe('Robot module', () => {
     // BUILD YOUR TESTS HERE
     
     // 1. Test that result is an array.
+    test('testing that result is an array', async () => {
+      expect(result).toHaveLength(2)
+        expect(typeof result).toMatch('object')
+      })
 
 
     // 2. Test that result[1] is of type object.
+    test('testing that result is an object', async () => {
+        expect(typeof result[1]).toMatch('object')
+      })
 
 
     describe('Validate report values', () => {
@@ -26,15 +33,30 @@ describe('Robot module', () => {
         const report = result[1]; 
 
         // 3. Test that report contains the properties: action, x, y, and facing.
-
+        // test('testing that report to contain properties: action, x,y,facing', async () => {
+        //   expect(report).toEqual(expect.arrayContaining(['Alice', 'Bob']))
+        // }
 
         // 4. Test that report.x is a number between 0 and 5.
+        test('testing that result.x between 0 and 5', async () => {
+            expect(report.x).toBeGreaterThanOrEqual(0)
+            expect(report.x).toBeLessThanOrEqual(5)
+          })
+        
 
 
         // 5. Test that report.y is a number between 0 and 5.
+        test('testing that result.y between 0 and 5', async () => {
+            expect(report.y).toBeGreaterThanOrEqual(0)
+            expect(report.y).toBeLessThanOrEqual(5)
+          })
+
 
 
         // 6. Test that report.facing is one of: NORTH, SOUTH, EAST, or WEST.
+        // test('testing that report.facing is one of: NORTH, SOUTH, EAST, WEST', async () => {
+        //   expect(report.facing).toEqual([expect.stringContaining('NORTH')])
+        //   })
 
     })
 })
